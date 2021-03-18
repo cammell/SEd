9cbcc1e9cd48c48c70e98622e085bcdc68bd508d
Cammell <cammell@gmail.com>
2021-03-18 22:16:18 +0100

    tests added, problems with sesionStorage js

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,20 @@ import '../scss/main.scss';
 const text=document.querySelector(".editor");
 const load=document.querySelector(".load");
 const save=document.querySelector(".save");
-
+localStorage.setItem('TEST','hard coded info');
 
 load.addEventListener('click', event => {
+    console.log("load pressed");
     if(!localStorage.getItem('text')) {
         text.value = localStorage.getItem('text');
     }
 })
 
 save.addEventListener('click', event => {
+    console.log("save pressed");
+    console.log(text.value);
     localStorage.setItem('text',text.value);
+    console.log('from save ' + text.value);
 })
 console.log(text);
 console.log(load);
